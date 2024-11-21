d149cdc1c9fa183fefd1f548450dfe389916d22d
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-11-21 19:27:08 +0530

    feat : added basic db and server setup

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	"example.com/rest-api/db"
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db.InitDB()
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createPost)
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createPost(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Something went wrong!!", "error": err})
+		return
+	}
+
+	event.ID = 1
+	event.UserId = 12
+
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
+}

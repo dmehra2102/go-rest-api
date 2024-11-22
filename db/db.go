986a12f7ed13cb9b4cad9441052abab297de475b
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-11-22 10:16:07 +0530

    feat : added event registration table to db

--- db/db.go
+++ db/db.go
@@ -52,4 +52,18 @@ func createTables() {
 	if err != nil {
 		panic("Could not able to create Event table")
 	}
+
+	createRegistrationsTable := `
+		CREATE TABLE IF NOT EXISTS registrations (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			event_id INTEGER,
+			user_id INTEGER,
+			FOREIGN KEY(event_id) REFERENCES events(id),
+			FOREIGN KEY(user_id) REFERENCES users(id)
+		)
+	`
+	_, err = DB.Exec(createRegistrationsTable)
+	if err != nil {
+		panic("Could not able to create Event registration table")
+	}
 }

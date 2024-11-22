f8416a08f6d569824b7eecc0d7c595f3b0ffc421
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-11-22 10:42:20 +0530

    feat : added route for cancelling registration

--- routes/register.go
+++ routes/register.go
@@ -34,4 +34,24 @@ func registerForEvent(context *gin.Context) {
 	context.JSON(http.StatusCreated, gin.H{"message": "Registered!!"})
 }
 
-func cancelRegisteration(context *gin.Context) {}
+func cancelRegisteration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id"})
+		return
+	}
+
+	var event models.Event
+	event.ID = eventId
+
+	err = event.CancelRegistration(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not register user for event."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Registeration cancelled!!"})
+}
